b416589c5fea3abc096ac9d725c34262e61a7224
Lucky Ali <151932520+Luckyali4@users.noreply.github.com>
2025-01-01 10:12:53 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,5 @@
+#Q3.Write a code for string,int and float input?
+name=input("enter your name:")
+age=int(input("enter your age:"))
+height=float(input("enter your height:"))
+print(f" name:{name},age:{age},height:{height}")
